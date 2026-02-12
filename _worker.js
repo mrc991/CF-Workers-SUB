fb94eec5a6bc14769d3c51d86eb7bc9a2d3098b2
mrc991 <lchen.forum@outlook.com>
2026-02-12 11:26:01 +0800

    Change greeting message from 'Hello' to 'Goodbye'

--- _worker.js
+++ _worker.js
@@ -17,8 +17,8 @@ https://cfxr.eu.org/getSub
 `;
 
 let urls = [];
-let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConverter = "subconvert.bigrich.cc"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConfig = "https://testingcf.jsdelivr.net/gh/mrc991/Custom_OpenClash_Rules@main/cfg/Custom_Clash.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
@@ -825,4 +825,5 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 			headers: { "Content-Type": "text/plain;charset=utf-8" }
 		});
 	}
+
 }
