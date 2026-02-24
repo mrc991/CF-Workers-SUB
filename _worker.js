add0f19438ee9de78dcc59115451facf2fae7650
mrc991 <lchen.forum@outlook.com>
2026-02-24 11:46:59 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- _worker.js
+++ _worker.js
@@ -13,7 +13,6 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-https://cfxr.eu.org/getSub
 `;
 
 let urls = [];
@@ -828,6 +827,7 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 	}
 
 }
+
 
 
 
